a4e7d711b15e7d3f08e2eef5b13d36be5905c7e5
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-16 18:35:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective (wrap = '*'){
+    const  innerFunction = function(name = "a hard worker"){
+return `You are ${wrap}${name}${wrap}!`;
+};
+return innerFunction;
+    };
